3af9af7ee953b9f298e47e0d9e1218d25b3c0267
Alexandr Mizev <mizzzev@outlook.com>
2022-08-03 16:27:09 +0300

    - created three methods(getAllBooks, addNewBook, add)

--- src/main/java/edu/app/controllers/BooksController.java
+++ src/main/java/edu/app/controllers/BooksController.java
@@ -1,9 +1,39 @@
 package edu.app.controllers;
 
+import edu.app.dao.BookDAO;
+import edu.app.models.Book;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/book")
 public class BooksController {
+
+    private final BookDAO bookDAO;
+    @Autowired
+    public BooksController(BookDAO bookDAO) {
+        this.bookDAO = bookDAO;
+    }
+
+    @GetMapping
+    public String getAllBooks(Model model){
+        model.addAttribute("books",bookDAO.getAllBooks());
+        return "booksPages/index";
+    }
+
+    @GetMapping("/add")
+    public String addNewBook(Model model){
+        model.addAttribute("book",new Book());
+        return "booksPages/newBook";
+    }
+
+    @PostMapping
+    public String add(Book book){
+        bookDAO.addBook(book);
+        return "redirect:/book";
+    }
 }
